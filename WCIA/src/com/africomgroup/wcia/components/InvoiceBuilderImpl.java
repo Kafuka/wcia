ee6bd01d8463a135f0046b7a4cd209115851958a
root <root@10.0.10.181>
2013-12-02 17:12:33 +0200

    commented out

--- WCIA/src/com/africomgroup/wcia/components/InvoiceBuilderImpl.java
+++ WCIA/src/com/africomgroup/wcia/components/InvoiceBuilderImpl.java
@@ -6,12 +6,6 @@
 
 package com.africomgroup.wcia.components;
 
-import com.africomgroup.wcia.dao.CustomerDAO;
-import com.africomgroup.wcia.dao.DAOException;
-import com.africomgroup.wcia.dao.DAOFactory;
-import com.africomgroup.wcia.entities.Customer;
-import com.africomgroup.wcia.entities.Invoice;
-
 /**
  *
  * @author Jordy Mugeni
@@ -19,30 +13,17 @@ import com.africomgroup.wcia.entities.Invoice;
 public class InvoiceBuilderImpl implements InvoiceBuilder{
     
 
-    @Override
-    public Invoice buildInvoice() {
+    //@Override
+    //public Invoice buildInvoice() {
         
-        CustomerDAO dao = DAOFactory.createCustomerDOA();
-        Customer customer = new Customer();
-        customer.setName("Judith");
-        customer.setSurname("Mugeni");
-        customer.setAddress("test");
-        customer.setId(16011);
-        
-        try {
-        dao.create(customer);
-        System.out.println (dao.findById("10"));
-        }catch (DAOException ex){
-            System.out.println (ex.getMessage());
-        
-        }
-        return null;
-    }
+ 
+      //  return null;
+   // }
     
     public static void main (String [] args){
         
         InvoiceBuilderImpl builder = new InvoiceBuilderImpl();
-        builder.buildInvoice();
+        //builder.buildInvoice();
         
     }
     
